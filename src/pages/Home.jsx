ac358a3ba6615eb81c2316434b6c2270c7a0f932
bilal Ahmed <bilalkietacc@outlook.com>
2024-10-02 11:08:12 +0500

    ui to show api data (incomplete)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ import "./Home.css";
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchBy, setSearchBy] = useState("name");
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const handleSearch = async () => {
     try {
@@ -21,10 +23,12 @@ const Home = () => {
       const response = await axios.get(endpoint);
 
       if (response.data.Response === "True") {
-        // setResults(response.data);
+        setResult(response.data);
+        setLoading(false);
         console.log("Search Results:", response.data);
       } else {
-        // setResults(null);
+        setResult(null);
+        setLoading(false);
         console.error("Movie not found:", response.data.Error);
       }
     } catch (error) {
@@ -35,42 +39,91 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="search-container">
-        <h1>Search Movies and Series</h1>
-        <div className="search-options">
-          <label>
-            <input
-              type="radio"
-              value="name"
-              checked={searchBy === "name"}
-              onChange={() => setSearchBy("name")}
-            />
-            Search by Name
-          </label>
-          <label>
-            <input
-              type="radio"
-              value="id"
-              checked={searchBy === "id"}
-              onChange={() => setSearchBy("id")}
-            />
-            Search by IMDB Id
-          </label>
+      <div className="container">
+        <div className="search-container">
+          <h1>Search Movies and Series</h1>
+          <div className="search-options">
+            <label>
+              <input
+                type="radio"
+                value="name"
+                checked={searchBy === "name"}
+                onChange={() => setSearchBy("name")}
+              />
+              Search by Name
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="id"
+                checked={searchBy === "id"}
+                onChange={() => setSearchBy("id")}
+              />
+              Search by Id
+            </label>
+          </div>
+          <input
+            type="text"
+            placeholder={`Enter ${
+              searchBy === "name" ? "movie name" : "movie ID"
+            }`}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input"
+          />
+          <button onClick={handleSearch} className="search-button">
+            Find it for me!
+          </button>
         </div>
-        <input
-          type="text"
-          placeholder={`Enter ${
-            searchBy === "name" ? "movie name" : "movie ID"
-          }`}
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="search-input"
-        />
-        <button onClick={handleSearch} className="search-button">
-          Find it for me!
-        </button>
-      </div>{" "}
-      <Footer></Footer>
+        <div className="results-container">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div className="results">
+              <h1 className="title">{result.Title}</h1>
+
+              <div className="content">
+                <div className="poster">
+                  <img src={result.Poster} alt={`${result.Title} Poster`} />
+                </div>
+
+                <div className="details">
+                  <p>
+                    <strong>Cast:</strong> {result.Actors}
+                  </p>
+                  <p>
+                    <strong>Genre:</strong> {result.Genre}
+                  </p>
+                  <p>
+                    <strong>Director:</strong> {result.Director}
+                  </p>
+                  <p>
+                    <strong>Writer:</strong> {result.Writer}
+                  </p>
+                  <p>
+                    <strong>Year:</strong> {result.Year}
+                  </p>
+                  <p>
+                    <strong>Runtime:</strong> {result.Runtime}
+                  </p>
+
+                  <div className="ratings">
+                    <strong>Ratings:</strong>
+                    <ul>
+                      {result.Ratings.map((rating, index) => (
+                        <li key={index}>
+                          {rating.Source}: {rating.Value}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
